bb980032f649eac3b9b68c94ef2254450c7ee15b
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-12-18 12:44:46 +0100

    upd

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from collatz import collatz_cycle
 from collatz import get_first_n_cycles
 from collatz import list_to_latex
 from collatz import list_first_20
+from sympy import primerange
+from sympy import factorint
 
-list_first_20()
+#list_first_20()
 
-
-
-
+for n in range(1, 40+1):
+    factors = factorint(n)
+    cycle = list(collatz_cycle(n))
+    has3 = 3 in factors
+    has2 = 2 in factors
+    print(n, has2, has3, factors, cycle)
